5d582aa6910a2f2c569ed1192b37c348762e9477
bitDel <facefrog1900@qq.com>
2025-05-27 20:46:40 +0800

    lruCache

--- lru_cache.hpp
+++ lru_cache.hpp
@@ -72,12 +72,73 @@ public:
     auto it = nodeMap_.find(key);
     if (it != nodeMap_.end()) {
       moveToMostRecent(it->second);
+      value = it->second->getValue();
       return true;
     }
     return false;
   }
 
+  Value get(Key key) override {
+    Value value{}; // 使用Value的默认构造函数
+    get(key, value);
+    return value;
+  }
+  
+  void remove(Key key) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = nodeMap_.find(key);
+    if (it != nodeMap_.end()) {
+      removeNode(it->second);
+      nodeMap_.erase(it);
+    }
+  }
 
+
+private:
+
+  void initalizeList() {
+    // dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
+    // dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+    // 显示构造临时临时对象，使用默认构造函数
+
+    dummyHead_ = std::make_shared<LruNodeType>(Key{}, Value{});
+    dummyTail_ = std::make_shared<LruNodeType>(Key{}, Value{});
+    dummyHead_->next_ = dummyTail_;
+    dummyTail_->prev_ = dummyHead_;
+  }
+
+  void updateExistingNode(NodePtr node, const Value &value) {
+    node->setValue(value);
+    moveToMostRecent(node);
+  }
+
+  void moveToMostRecent(NodePtr node) {
+    removeNode(node);
+    insertNode(node);
+  }
+
+  void removeNode(NodePtr node) {
+    if (!node->prev_.expired() && node->next_) {
+      auto prev = node->prev_.lock();
+      prev->next_ = node->next_;
+      node->next_->prev_ = prev;
+      node->next_ = nullptr;
+      // 清空指针，断开连接
+    }
+  }
+
+  void insertNode(NodePtr node) {
+    node->next_ = dummyTail_;
+    node->prev_ = dummyTail_->prev_;
+    dumyyTail_->prev_.lock()->next_ = node;
+    dummyTail_->prev_ = node;
+  }
+
+  void evictLeastRecent() {
+    NodePtr leastRecent = dummyHead_->next_;
+    removeNode(leastRecent);
+    nodeMap_.erase(leastRecent->getKey());
+  }
 
 private:
   int capacity_;
