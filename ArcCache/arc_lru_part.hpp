d8065d561f4e0ad7d682a4ffcabf30e0ef744199
bitDel <facefrog1900@qq.com>
2025-06-07 16:44:02 +0800

    arc_lru_part.hpp is done

--- ArcCache/arc_lru_part.hpp
+++ ArcCache/arc_lru_part.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "arc_cache_node.hpp"
-
 #include <unordered_map>
 #include <mutex>
 
@@ -31,13 +30,46 @@ public:
     std::lock_guard<std::mutex> lock(mutex_);
     auto it = mainCache_.find(key);
     if (it != mainCache_.end()) {
-      return updateExi
+      return updateExistingNode(it->second, value);
     }
+    return addNewNode(key, value);
   }
 
+  bool get(Key key, Value &value, bool &shouldTransform) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = mainCache_.find(key);
+    if (it != mainCache_.end()) {
+      shouldTransform = updateNodeAccess(it->second);
+      value = it->second->getValue();
+      return true;
+    }
+    return false;
+  }
 
+  bool checkGhost(Key key) {
+    auto it = ghostCache_.find(key);
+    if (it != ghostCache_.end()) {
+      removeFromGhost(it->second);
+      ghostCache_.erase(it);
+      return true;
+    }
+    return false;
+  }
 
-  
+  void increaseCapacity() {
+    capacity_ ++;
+  }
+
+  bool decreaseCapacity() {
+    if (capacity_ <= 0) {
+      return false;
+    }
+    if (mainCache_.size() == capacity_) {
+      evictLeastRecent();
+    }
+    capacity_ --;
+    return true;
+  }
 
 private:
   void initializeList() {
@@ -50,6 +82,56 @@ private:
     ghostHead_ = std::make_shared<NodeType>();
     ghostHead_->next_ = ghostTail_;
     ghostTail_->prev_ = ghostHead_;
+  }
+
+  bool addNewNode(const Key &key, const Value &value) {
+    if (mainCache_.size() >= capacity_) {
+      evictLeastRecent();
+    }
+
+    NodePtr newNode = std::make_shared<NodePtr>(key, value);
+    mainCache_[key] = newNode;
+    addToFront(newNode);
+    return true;
+  }
+
+  bool updateNodeAccess(NodePtr node) {
+    moveToFront(node);
+    node->incrementAccessCount();
+    return node->getAccessCount() >= transformThreshold_;
+  }
+
+  bool updateExistingNode(NodePtr node, const Value &value) {
+    node->setValue(value);
+    moveToFront(node);
+    return true;
+  }
+
+  void moveToFront(NodePtr node) {
+    if (!node->prev_.expired() && node->next_) {
+      auto prev = node->prev_.lock();
+      prev->next_ = node->next_;
+      node->next_->prev_ = node->prev_;
+      node->next_ = nullptr;
+    }
+
+    addToFront(node);
+  }
+
+  void evictLeastRecent() {
+    NodePtr leastRecent = mainTail_->prev_.lock();
+    if (!leastRecent || leastRecent == mainHead_) {
+      return;
+    }
+
+    removeFromMain(leastRecent);
+
+    if (ghostCache_.size() >= ghostCapacity_) {
+      removeOldestGhost();
+    }
+
+    addToGhost(leastRecent);
+    mainCache_.erase(leastRecent->getKey());
   }
 
   void removeOldestGhost() {
@@ -89,17 +171,12 @@ private:
     ghostCache_[node->getKey()] = node;
   }
 
-
-
-
-
-
-
-
-
-
-
-
+  void addToFront(NodePtr node) {
+    node->next_ = mainHead_->next_;
+    node->prev_ = mainHead_;
+    mainHead_->next_->prev_ = node;
+    mainHead_->next_ = node;
+  }
 
 private:
   size_t capacity_;
